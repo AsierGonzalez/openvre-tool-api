7a70e4a722c0616cd38181a3b25a305b2831eb7f
Laura Rodríguez Navas <lrodrin@users.noreply.github.com>
2020-03-16 16:22:50 +0100

    commented Cannot import \"pycompss\" API packages

--- basic_modules/tool.py
+++ basic_modules/tool.py
@@ -26,8 +26,8 @@ try:
     from pycompss.api.parameter import FILE_IN, FILE_OUT
     from pycompss.api.task import task
 except ImportError:
-    print("[Warning] Cannot import \"pycompss\" API packages.")
-    print("          Using mock decorators.")
+    # print("[Warning] Cannot import \"pycompss\" API packages.")
+    # print("          Using mock decorators.")
 
     from utils.dummy_pycompss import FILE_IN, FILE_OUT
     from utils.dummy_pycompss import task
